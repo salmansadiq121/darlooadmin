7ff23c37073f62e71af0311bfa3996c723e6240c
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-12 12:53:31 +0500

    latest updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
+    domains: ["*"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**",
       },
-      {
-        protocol: "https",
-        hostname: "secure.gravatar.com",
-      },
-      {
-        protocol: "https",
-        hostname: "flagcdn.com",
-      },
-      {
-        protocol: "https",
-        hostname: "**.alicdn.com", // supports cbu01.alicdn.com and others
-      },
-      {
-        protocol: "https",
-        hostname: "s3.amazonaws.com", // example AWS
-      },
-      {
-        protocol: "https",
-        hostname: "*.amazonaws.com", // wildcard AWS bucket domains
-      },
-      {
-        protocol: "https",
-        hostname: "*.alibaba.com", // if you also need alibaba images
-      },
     ],
+    // unoptimized: true,
   },
   reactStrictMode: true,
 };
