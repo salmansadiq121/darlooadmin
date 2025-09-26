c64cb4e7d92f309e1e9472c921d83f51b8c6a9e7
Salman Sadiq <salmansadiq1015@gmail.com>
2025-09-26 13:39:25 +0500

    size chart , varient prices

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,56 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   images: {
+    domains: ["*"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "cbu01.alicdn.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.alicdn.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "secure.gravatar.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "**",
       },
     ],
-    unoptimized: true,
+    // unoptimized: false,
   },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
