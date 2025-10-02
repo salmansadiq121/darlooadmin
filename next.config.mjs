15653627889db4ab8a9817d9fb89751e19ce76e3
Salman Sadiq <salmansadiq1015@gmail.com>
2025-10-02 14:08:53 +0500

    latest updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,97 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
   images: {
-    unoptimized: true,
+    domains: ["*"],
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "darloo.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "secure.gravatar.com",
-      },
-      {
-        protocol: "https",
-        hostname: "flagcdn.com",
+        hostname: "**",
       },
     ],
+    unoptimized: true,
   },
-
-  webpack: (config, context) => {
-    config.externals.push({
-      "thread-stream": "commonjs thread-stream",
-    });
-    return config;
-  },
-  staticPageGenerationTimeout: 280,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   },
 //   images: {
-//     domains: ["*"],
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//       },
-//     ],
 //     unoptimized: true,
-//   },
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
-// ------------------------------------------------------------------
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
 //     remotePatterns: [
 //       {
 //         protocol: "https",
-//         hostname: "cbu01.alicdn.com", // 1688 images
+//         hostname: "darloo.com",
+//         port: "",
+//         pathname: "/**",
 //       },
 //       {
 //         protocol: "https",
-//         hostname: "s3.eu-north-1.amazonaws.com", // AWS S3
+//         hostname: "secure.gravatar.com",
 //       },
 //       {
 //         protocol: "https",
-//         hostname: "your-cloudfront-domain.cloudfront.net", // CloudFront
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "cdn.shopify.com",
-//         pathname: "**",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "avatars.githubusercontent.com", // GitHub avatars
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "raw.githubusercontent.com", // GitHub raw files
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "user-images.githubusercontent.com", // GitHub repo assets
+//         hostname: "flagcdn.com",
 //       },
 //     ],
 //   },
-//   reactStrictMode: true,
+
+//   webpack: (config, context) => {
+//     config.externals.push({
+//       "thread-stream": "commonjs thread-stream",
+//     });
+//     return config;
+//   },
+//   staticPageGenerationTimeout: 280,
 // };
 
 // export default nextConfig;
